a0a56ce72cc7e9d5b7bb4e21d7425bf5266a64c0
Itay Levy <itaylevy134@gmail.com>
2020-04-08 04:55:58 +0300

    refactored to use default mocha tests syntax

---
+++ test/tests1/testUser.ts
@@ -0,0 +1,54 @@
+import * as firebase from "@firebase/testing";
+import {
+	authedApp,
+	createLegalUserDocument,
+	phoneNumber,
+	usersCollection,
+} from "../utils";
+
+describe('Test user security rules',
+	() => {
+		it('should successfully create user document', async function () {
+			await firebase.assertSucceeds(
+				createLegalUserDocument()
+			);
+		});
+		it('should fail to create user document without name', async function () {
+			await firebase.assertFails(usersCollection.add(
+				{
+					phoneNumber: phoneNumber
+				}
+			));
+		});
+		it('should fail to create illegal user document with wrong phone number', async function () {
+			await firebase.assertFails(usersCollection.add(
+				{
+					phoneNumber: '12345',
+					name: 'Itay Levy',
+				}
+			));
+		});
+		it('should successfully delete user document', async function () {
+			const createdDocument = await createLegalUserDocument();
+			await firebase.assertSucceeds(
+				createdDocument.delete()
+			);
+		});
+		it('should successfully read user document', async function () {
+			const createdDocument = await createLegalUserDocument();
+			await firebase.assertSucceeds(
+				createdDocument.get()
+			);
+		});
+		it('should fail to read user document when user not authenticated', async function () {
+			await createLegalUserDocument();
+			const unauthenticatedApp = authedApp({uid: 'wrongUid', phone_number: 'wrong phone number'});
+			await firebase.assertFails(
+				unauthenticatedApp
+					.collection('users')
+					.where('phone_number', '==', phoneNumber)
+					.get()
+			);
+		});
+	}
+);
---
+++ test/tests2/testSentMessages.ts
@@ -0,0 +1,20 @@
+import * as firebase from "@firebase/testing";
+import {coverageUrl, db, projectId, rules, userID} from "../utils";
+//
+// @suite
+// class TestSentMessagesSecurityRules {
+//
+// 	@test
+// 	async "create legal sent message"() {
+// 		// firebase.assertSucceeds(
+// 		// 	db.collection('users')
+// 		// 		.where(name)
+// 		// 		.collection('sentMessages')
+// 		// 		.add()
+// 		// )
+// 		console.log('ran');
+// 	}
+// }
+//
+
+//
