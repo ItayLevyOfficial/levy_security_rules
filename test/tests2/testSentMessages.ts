f0fbf63793fbba16f94aeac94974832aea1ac6fb
Itay Levy <itaylevy134@gmail.com>
2020-04-09 04:38:55 +0300

    all tests passes

--- test/tests2/testSentMessages.ts
+++ test/tests2/testSentMessages.ts
@@ -31,5 +31,26 @@ describe('Test the sent messages collection security rules', () => {
 				)
 			);
 		});
+		it('should fail to create a sent message with wrong created_at', async function () {
+			await firebase.assertFails(
+				sentMessages.add(
+					{
+						text: 'Test text',
+						// This date is wrong because it's the client date, not the server one.
+						created_at: +new Date()
+					}
+				)
+			);
+		});
+		it('should fail to create a sent message too long text', async function () {
+			await firebase.assertFails(
+				sentMessages.add(
+					{
+						text: 'a'.repeat(501),
+						created_at: firebase.database.ServerValue.TIMESTAMP
+					}
+				)
+			);
+		});
 	}
 );
